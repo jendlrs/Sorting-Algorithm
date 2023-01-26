bfd8c0a37df92e5c1443b3493b72f481b5695013
unknown <jensennics.dlrs@gmail.com>
2023-01-26 13:54:21 +0800

    Listed how bubble sort works and assigned the array values to be sort.

---
+++ bubble_sort.py
@@ -0,0 +1,8 @@
+#Bubble Sort
+
+#Step 1: Compare two adjacent elements at a time
+#Step 2: Swap if the element in first index is greater than the second index. Else retain.
+#Step 3: Repeat until there is no swap is required and all elements come to its correct position.
+
+#Assigned the array values
+numbers = [41, 4, 47, 94, 40, 66, 42, 23, 60, 55]
