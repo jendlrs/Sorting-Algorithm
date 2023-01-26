8c98e90447bb9bc9841ceab0a55d542fcb5d32bb
unknown <jensennics.dlrs@gmail.com>
2023-01-26 17:11:28 +0800

    Listed how merge sort works and assigned the array values to be sorted.

---
+++ merge_sort.py
@@ -0,0 +1,8 @@
+#Merge Sort
+
+#Step 1: Divide array in half
+#Step 2: Call the merge sort for each half to sort them
+#Step 3: Merge both sorted halves to have one sorted array
+
+#Assigned the array values
+numbers = [41, 4, 47, 94, 40, 66, 42, 23, 60, 55]
