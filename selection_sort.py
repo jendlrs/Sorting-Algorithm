58e62cbd3abac073dfb5511ffffab181ad40dbe6
unknown <jensennics.dlrs@gmail.com>
2023-01-26 13:12:13 +0800

    Defined the selection sort function and added for loops that will check every value in the array. Next step is to swap the smallest element to the index where the search started.

--- selection_sort.py
+++ selection_sort.py
@@ -6,3 +6,11 @@
 
 #Assigned the array values
 numbers = [41, 4, 47, 94, 40, 66, 42, 23, 60, 55]
+
+def selection_sort (numbers): #This will check every value in the array
+    for i in range (0, len(numbers) - 1): 
+        current_min_index = i 
+        for j in range (i + 1, len (numbers)):
+            if numbers[j] < numbers [current_min_index]: 
+                current_min_index = j #changed the minimum index 
+
