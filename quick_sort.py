2c8954162f0d49755b0a0f9a7b074c8c4e1187d3
unknown <jensennics.dlrs@gmail.com>
2023-01-26 21:55:01 +0800

    Listed how the quick sort works and assigned the array values to be sorted.

---
+++ quick_sort.py
@@ -0,0 +1,11 @@
+#Quick Sort 
+
+#Step 1: Choose a pivot element (usually last or random)
+#Step 2: Stores elements less than pivot in left subarray
+       # Stores elements less than pivot in right subarray
+#Step 3: Call quick sort recursively on left subarray
+       # Call quick sort recursively on left subarray
+#Step 4: Repeat until all the elements come to its correct position.
+
+#Assigned the array values
+numbers = [41, 4, 47, 94, 40, 66, 42, 23, 60, 55]
