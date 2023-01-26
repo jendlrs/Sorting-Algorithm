72aac79ed809914d746a5a3bf4ee98f4a6325413
unknown <jensennics.dlrs@gmail.com>
2023-01-26 16:23:00 +0800

    Listed how insertion sort works and assigned the array values.

---
+++ insertion_sort.py
@@ -0,0 +1,8 @@
+#Insertion Sort
+
+#Step 1: Divide the array into two parts: sorted and unsorted. For the first iteration, only the first element is in the sorted part
+#Step 2: Get the first element in the unsorted part, then compare the values, swap until it reaches the correct position.
+#Step 3: Repeat until all the numbers are sorted.
+
+#Assigned the array values
+numbers = [41, 4, 47, 94, 40, 66, 42, 23, 60, 55]
