7d3786dffaeaddbbbba024ddb675cfabf34749d1
unknown <jensennics.dlrs@gmail.com>
2023-01-26 13:01:04 +0800

    Listed how the selection sort works and also assigned the array values.

---
+++ selection_sort.py
@@ -0,0 +1,8 @@
+#Selection Sort
+
+#Step 1: Find the smallest element in the array
+#Step 2: Swap where the search started 
+#Step 3: Repeat until all the elements come to its correct position.
+
+#Assigned the array values
+numbers = [41, 4, 47, 94, 40, 66, 42, 23, 60, 55]
