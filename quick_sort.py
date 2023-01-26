bc12d2cf2de055a9150f51a801519b023e180a5b
unknown <jensennics.dlrs@gmail.com>
2023-01-26 22:03:10 +0800

    Defined the quick sort function. The next step is to complete the partition function.

--- quick_sort.py
+++ quick_sort.py
@@ -9,3 +9,11 @@
 
 #Assigned the array values
 numbers = [41, 4, 47, 94, 40, 66, 42, 23, 60, 55]
+
+def quick_sort (numbers, left, right):
+    if left < right:
+        partition_pos = partition (numbers, left, right)
+        quick_sort (numbers, left, partition_pos -1)
+        quick_sort (numbers, partition_pos + 1, right)
+
+def partition(numbers, left, right):
